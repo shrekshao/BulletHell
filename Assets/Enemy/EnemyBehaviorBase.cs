01f16c1d851e6cfc450df06ef7b8c90adc50a7c6
shrekshao <shrekshao@gmail.com>
2016-04-02 19:36:08 -0400

    aim, itween path follow

---
+++ Assets/Enemy/EnemyBehaviorBase.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviorBase : MonoBehaviour {
+
+
+    [SerializeField] Sprite bullet;
+
+    Transform fireTransform;
+
+    //temp
+    Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+        fireTransform = transform.Find("fire_position");
+        playerTransform = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Aim(playerTransform);
+	}
+
+    void Fire()
+    {
+        Instantiate(bullet, fireTransform.position, this.transform.rotation);
+    }
+
+    void Aim(Transform target)
+    {
+        float r = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, r));
+    }
+}
---
+++ Assets/Enemy/EnemyMovePath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovePath : MonoBehaviour {
+
+    [SerializeField]
+    string pathName = "New Path 1";
+
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "time", 5));
+    }
+	
+}
