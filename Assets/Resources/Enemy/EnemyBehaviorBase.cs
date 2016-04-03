969be8e6c8a3eb6f42b083d475d9dda8f9bca8f3
shrekshao <shrekshao@gmail.com>
2016-04-03 00:42:40 -0400

    bullet enemy

--- Assets/Resources/Enemy/EnemyBehaviorBase.cs
+++ Assets/Resources/Enemy/EnemyBehaviorBase.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class EnemyBehaviorBase : MonoBehaviour {
 
 
-    [SerializeField] Sprite bullet;
+    //[SerializeField] Sprite bullet;
 
     Transform fireTransform;
 
@@ -20,12 +20,43 @@ public class EnemyBehaviorBase : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Aim(playerTransform);
+
+        if(Random.value < 0.05)
+        {
+            Fire();
+        }
 	}
+
+
+
+    const float BulletSpeed = 0.2f;
+    const int BulletPeriod = 1;
+    const float BulletSpread = 0.15f;
+
+    public Bullet prefab;
+
+    float since_last_shot = 0.0f;
+    float last_angle = 0.0f;
 
     void Fire()
     {
-        Instantiate(bullet, fireTransform.position, this.transform.rotation);
+        //Instantiate(bullet, fireTransform.position, this.transform.rotation);
+
+        Vector2 vel = new Vector2(last_angle - BulletSpread * 0.5f, 1.0f);
+        last_angle = (last_angle + BulletSpread * 0.618f) % BulletSpread;
+        vel.Normalize();
+        vel *= BulletSpeed;
+
+        float r = (this.transform.rotation.eulerAngles.z - 90.0f) * Mathf.Deg2Rad;
+        Vector2 tmp = vel;
+        vel.x = Mathf.Cos(r) * tmp.x - Mathf.Sin(r) * tmp.y;
+        vel.y = Mathf.Sin(r) * tmp.x + Mathf.Cos(r) * tmp.y;
+
+
+        BulletArena.Spawn(prefab, fireTransform.position, vel, true);
+        since_last_shot = 0;
     }
+
 
     void Aim(Transform target)
     {
